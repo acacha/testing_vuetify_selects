e8ba14ab4674a5a2316576525a34adde5b6a9547
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-10-10 11:39:22 +0200

    first commit

--- tests/unit/helloworld.spec.js
+++ tests/unit/helloworld.spec.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
-import { shallowMount, mount } from '@vue/test-utils'
-import Prova from '../../src/components/Prova'
+import { shallowMount, mount, createWrapper } from '@vue/test-utils'
+import Specialties from '../../src/components/Specialties'
 import Router from 'vue-router'
 import Vuetify from 'vuetify'
 import Vue from 'vue'
@@ -8,32 +8,23 @@ import Vue from 'vue'
 Vue.use(Router)
 Vue.use(Vuetify)
 
-// import sv from 'i18n/vuetify/sv'
-//
-// Vue.use(Vuetify, {
-//   lang: {
-//     locales: { sv },
-//     current: 'sv'
-//   }
-// })
+// const divData = document.createElement('div')
+// divData.setAttribute('data-app', true)
+// document.body.appendChild(divData)
 
-const el = document.createElement('div')
-el.setAttribute('data-app', true)
-document.body.appendChild(el)
-
-describe('Prova', () => {
+describe('Specialties', () => {
   let props
   let vuetify
 
   // eslint-disable-next-line no-unused-vars
   const build = (properties) => {
     properties = properties || props
-    const wrapper = shallowMount(Prova, {
+    const wrapper = shallowMount(Specialties, {
       propsData: properties,
       vuetify
       // sync: false
     })
-    const wrapperMounted = mount(Prova, {
+    const wrapperMounted = mount(Specialties, {
       propsData: properties,
       vuetify
       // sync: false
@@ -46,8 +37,10 @@ describe('Prova', () => {
 
   beforeEach(() => {
     props = {
-      specialties: ['Item 1', 'Item 2']
+      specialties: ['Specialty 1', 'Specialty 2']
     }
+    // MOCKING t to avoid undefined error
+    // TODO https://github.com/vuetifyjs/vuetify/blob/ac5c6bf353a50e9b2019374b3948e9b7a98b0b30/packages/vuetify/src/components/VAlert/__tests__/VAlert.spec.ts
     vuetify = new Vuetify({
       mocks: {
         $vuetify: {
@@ -59,33 +52,17 @@ describe('Prova', () => {
     })
   })
 
-  it('Renders the mounted component prova 222', async () => {
-    // TODO https://github.com/vuetifyjs/vuetify/blob/ac5c6bf353a50e9b2019374b3948e9b7a98b0b30/packages/vuetify/src/components/VAlert/__tests__/VAlert.spec.ts
-    const { wrapperMounted, wrapper } = build()
-
+  it('Renders the specialities select', async () => {
+    const { wrapperMounted } = build()
     await wrapperMounted.vm.$nextTick()
 
-    console.log(wrapperMounted.html())
+    // NOT VALID: PARENT IS CHILD HIMSELF
+    // console.log(createWrapper(wrapperMounted.vm.$parent).html())
+    // ALSO DOES NOT WORK
+    // console.log(createWrapper(divData).html())
+    // console.log(wrapperMounted.html())
 
-    await wrapper.vm.$nextTick()
-
-    console.log(wrapper.html())
+    // NOT WORKING VUETIFY RENDER SELECT LIST OUTSIDE COMPONENT DOM
+    expect(wrapperMounted.html()).toMatch('Specialty 1')
   })
 })
-
-// beforeEach(() => {
-//   mountFunction = (options = {}) => {
-//     return mount(VAlert, {
-//       ...options,
-//       // https://github.com/vuejs/vue-test-utils/issues/1130
-//       sync: false,
-//       mocks: {
-//         $vuetify: {
-//           lang: {
-//             t: (val: string) => val,
-//           },
-//         },
-//       },
-//     })
-//   }
-// })
